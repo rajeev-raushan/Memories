5ada764f0062ae520cb205ab5d0ff40b39429190
rajeev-raushan <95717011+rajeev-raushan@users.noreply.github.com>
2022-11-26 05:02:02 +0530

    created backend server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,31 @@
+import dotenv from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cros from "cros";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+app.use("/posts", postRoutes);
+const CONNECTION_URL = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, process.env.RESTREVIEWS_DB_URI, {
+    useNewUrlParser: true,
+    useUNifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server is running on port:${PORT} `))
+  )
+  .catch((error) => console.log(error.message));
+
+// mongoose.set(useFindAndModify, false);
